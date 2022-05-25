d9f291a94a4d2a588fbc9213aedd22457c5ad3bd
YinanZhao <64669960+YinanZhao@users.noreply.github.com>
2022-05-25 00:28:05 -0400

    added submit button

--- src/components/sections/Cta.js
+++ src/components/sections/Cta.js
@@ -1,8 +1,33 @@
-import React from 'react';
+import React, { useState } from 'react';
 import PropTypes from 'prop-types';
 import classNames from 'classnames';
 import { SectionProps } from '../../utils/SectionProps';
-import Input from '../elements/Input';
+import { FaRegArrowAltCircleRight } from "react-icons/fa";
+
+// firebase
+import { initializeApp } from "firebase/app";
+import { getFirestore } from 'firebase/firestore';
+import { collection , addDoc } from 'firebase/firestore';
+const firebaseConfig = {
+  apiKey: "AIzaSyCB1omIp2ZyNOkbTE5S6LA27ZoTEuilJcw",
+  authDomain: "wotto-d26be.firebaseapp.com",
+  projectId: "wotto-d26be",
+  storageBucket: "wotto-d26be.appspot.com",
+  messagingSenderId: "195811865823",
+  appId: "1:195811865823:web:c31992074ae457ef87a56a",
+  measurementId: "G-R7CS15R5S3"
+};
+// firebase
+// Import the functions you need from the SDKs you need
+// import firebase from "firebase/compat/app";
+// import db from "firebase";
+// import 'firebase/firestore'
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+
+// Initialize Firebase
+// const db = firebase.firestore()
 
 const propTypes = {
   ...SectionProps.types,
@@ -42,6 +67,20 @@ const Cta = ({
     split && 'cta-split'
   );  
 
+  const [email, setEmail] = useState("");
+
+  const firebaseApp = initializeApp(firebaseConfig);
+  const db = getFirestore(firebaseApp);
+
+  // add new email
+  async function onSubscribe() {
+    console.log(email)
+    const data = {
+      email: email
+    };
+    addDoc(collection(db,'subscribers'), data)
+  };
+
   return (
     <section
       {...props}
@@ -57,11 +96,18 @@ const Cta = ({
               </h3>
           </div>
           <div className="cta-action">
-            <Input id="newsletter" type="email" label="Subscribe" labelHidden hasIcon="right" placeholder="Your best email">
-              <svg width="16" height="12" xmlns="http://www.w3.org/2000/svg">
-                <path d="M9 5H1c-.6 0-1 .4-1 1s.4 1 1 1h8v5l7-6-7-6v5z" fill="#376DF9" />
-              </svg>
-            </Input>
+          <form>
+            <label>Enter your name:
+              <input
+                type="text" 
+                value={email}
+                onChange={(e) => setEmail(e.target.value)}
+              />
+            </label>
+          </form>
+            <button style = {{backgroundColor: "transparent", borderRadius: 0}} onClick = {onSubscribe}>
+              <FaRegArrowAltCircleRight/>
+            </button>
           </div>
         </div>
       </div>
