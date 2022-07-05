a99923b99f81a7d07368c1a8c9f2ca82be7af1b6
Steven11876 <63318691+Steven11876@users.noreply.github.com>
2022-07-05 18:12:02 -0400

    test
    testing

--- src/components/sections/FeaturesSplit.js
+++ src/components/sections/FeaturesSplit.js
@@ -1,16 +1,16 @@
-import React from 'react';
-import classNames from 'classnames';
-import { SectionSplitProps } from '../../utils/SectionProps';
-import SectionHeader from './partials/SectionHeader';
-import Image from '../elements/Image';
+import React from "react";
+import classNames from "classnames";
+import { SectionSplitProps } from "../../utils/SectionProps";
+import SectionHeader from "./partials/SectionHeader";
+import Image from "../elements/Image";
 
 const propTypes = {
-  ...SectionSplitProps.types
-}
+  ...SectionSplitProps.types,
+};
 
 const defaultProps = {
-  ...SectionSplitProps.defaults
-}
+  ...SectionSplitProps.defaults,
+};
 
 const FeaturesSplit = ({
   className,
@@ -26,128 +26,140 @@ const FeaturesSplit = ({
   imageFill,
   ...props
 }) => {
-
   const outerClasses = classNames(
-    'features-split section',
-    topOuterDivider && 'has-top-divider',
-    bottomOuterDivider && 'has-bottom-divider',
-    hasBgColor && 'has-bg-color',
-    invertColor && 'invert-color',
+    "features-split section",
+    topOuterDivider && "has-top-divider",
+    bottomOuterDivider && "has-bottom-divider",
+    hasBgColor && "has-bg-color",
+    invertColor && "invert-color",
     className
   );
 
   const innerClasses = classNames(
-    'features-split-inner section-inner',
-    topDivider && 'has-top-divider',
-    bottomDivider && 'has-bottom-divider'
+    "features-split-inner section-inner",
+    topDivider && "has-top-divider",
+    bottomDivider && "has-bottom-divider"
   );
 
   const splitClasses = classNames(
-    'split-wrap',
-    invertMobile && 'invert-mobile',
-    invertDesktop && 'invert-desktop',
-    alignTop && 'align-top'
+    "split-wrap",
+    invertMobile && "invert-mobile",
+    invertDesktop && "invert-desktop",
+    alignTop && "align-top"
   );
 
   const sectionHeader = {
-    title: 'Workflow that just works',
-    paragraph: 'Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum — semper quis lectus nulla at volutpat diam ut venenatis.'
+    title: "Workflow that just works",
+    paragraph:
+      "Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum — semper quis lectus nulla at volutpat diam ut venenatis.",
   };
 
   return (
-    <section
-      {...props}
-      className={outerClasses}
-    >
+    <section {...props} className={outerClasses}>
       <div className="container">
         <div className={innerClasses}>
           {/*<SectionHeader data={sectionHeader} className="center-content" />*/}
           <div className={splitClasses}>
-
             <div className="split-item">
-              <div className="split-item-content center-content-mobile reveal-from-left" data-reveal-container=".split-item">
+              <div
+                className="split-item-content center-content-mobile reveal-from-left"
+                data-reveal-container=".split-item"
+              >
                 <div className="text-xxs text-color-primary fw-600 tt-u mb-8">
                   First Step
-                  </div>
-                <h3 className="mt-0 mb-12">
-                  Save Money in any Account
-                  </h3>
+                </div>
+                <h3 className="mt-0 mb-12">Save Money in any Account</h3>
                 <p className="m-0">
-                  Just save money to any savings account. Then, connect your savings account to Wotto for us to verify that you have deposited the money. 
-                  </p>
+                  Just save money to any savings account. Then, connect your
+                  savings account to Wotto for us to verify that you have
+                  deposited the money.
+                </p>
               </div>
-              <div className={
-                classNames(
-                  'split-item-image center-content-mobile reveal-from-bottom',
-                  imageFill && 'split-item-image-fill'
+              <div
+                className={classNames(
+                  "split-item-image center-content-mobile reveal-from-bottom",
+                  imageFill && "split-item-image-fill"
                 )}
-                data-reveal-container=".split-item">
+                data-reveal-container=".split-item"
+              >
                 <Image
-                  src={require('./../../assets/images/saving.jpeg')}
+                  src={require("./../../assets/images/saving.jpeg")}
                   alt="Features split 01"
                   width={528}
-                  height={396} />
+                  height={396}
+                />
               </div>
             </div>
 
             <div className="split-item">
-              <div className="split-item-content center-content-mobile reveal-from-right" data-reveal-container=".split-item">
+              <div
+                className="split-item-content center-content-mobile reveal-from-right"
+                data-reveal-container=".split-item"
+              >
                 <div className="text-xxs text-color-primary fw-600 tt-u mb-8">
                   Second Step
-                  </div>
-                <h3 className="mt-0 mb-12">
-                  Get Rewarded Scratch Tickets
-                  </h3>
+                </div>
+                <h3 className="mt-0 mb-12">Get Rewarded Scratch Tickets</h3>
                 <p className="m-0">
-                  On the Wotto app, you will recieve 1 token for every dollar that you have saved. With these tokens, you can exchange them for scratch tickets. Different tiers of scratch tickets will cost varying amounts of tokens and have varying rewards. 
-                  </p>
+                  On the Wotto app, you will recieve 1 token for every 25 dollar
+                  that you have saved for a month. With these tokens, you can
+                  exchange them for scratch tickets. Different tiers of scratch
+                  tickets will cost varying amounts of tokens and have varying
+                  rewards.
+                </p>
               </div>
-              <div className={
-                classNames(
-                  'split-item-image center-content-mobile reveal-from-bottom',
-                  imageFill && 'split-item-image-fill'
+              <div
+                className={classNames(
+                  "split-item-image center-content-mobile reveal-from-bottom",
+                  imageFill && "split-item-image-fill"
                 )}
-                data-reveal-container=".split-item">
+                data-reveal-container=".split-item"
+              >
                 <Image
-                  src={require('./../../assets/images/tickets.png')}
+                  src={require("./../../assets/images/tickets.png")}
                   alt="Features split 02"
                   width={528}
-                  height={396} />
+                  height={396}
+                />
               </div>
             </div>
 
             <div className="split-item">
-              <div className="split-item-content center-content-mobile reveal-from-left" data-reveal-container=".split-item">
+              <div
+                className="split-item-content center-content-mobile reveal-from-left"
+                data-reveal-container=".split-item"
+              >
                 <div className="text-xxs text-color-primary fw-600 tt-u mb-8">
                   Third Step
-                  </div>
-                <h3 className="mt-0 mb-12">
-                  Have Chance at Big Rewards
-                  </h3>
+                </div>
+                <h3 className="mt-0 mb-12">Have Chance at Big Rewards</h3>
                 <p className="m-0">
-                   After one month, the scratch tickets that you have traded tokens for are unlocked. Scratch them for a chance at big rewards, and keep saving to unlock more. 
-                  </p>
+                  Scratch your Wotto scratch tickets for a chance at big
+                  rewards, and keep saving to unlock more. The money you save
+                  will continue to produce tokens month to month.
+                </p>
               </div>
-              <div className={
-                classNames(
-                  'split-item-image center-content-mobile reveal-from-bottom',
-                  imageFill && 'split-item-image-fill'
+              <div
+                className={classNames(
+                  "split-item-image center-content-mobile reveal-from-bottom",
+                  imageFill && "split-item-image-fill"
                 )}
-                data-reveal-container=".split-item">
+                data-reveal-container=".split-item"
+              >
                 <Image
-                  src={require('./../../assets/images/winning.jpeg')}
+                  src={require("./../../assets/images/winning.jpeg")}
                   alt="Features split 03"
                   width={528}
-                  height={396} />
+                  height={396}
+                />
               </div>
             </div>
-
           </div>
         </div>
       </div>
     </section>
   );
-}
+};
 
 FeaturesSplit.propTypes = propTypes;
 FeaturesSplit.defaultProps = defaultProps;
